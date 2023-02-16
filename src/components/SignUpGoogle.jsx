99fe9341f1ba87d36e00feb106cc71dfbb8b5204
Elvio Galeano <elvio.galeano827@gmail.com>
2023-02-16 18:46:45 -0300

    create component SignUpGoogle

---
+++ src/components/SignUpGoogle.jsx
@@ -0,0 +1,46 @@
+import jwtDecode from 'jwt-decode'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSignUpMutation } from '../features/usersApi'
+export default function SignUpGoogle() {
+    const [user,setUser] = useState({})
+    const [signup] = useSignUpMutation()
+    const navigate = useNavigate()
+    function handleCallbackResponse(response){
+        console.log("token " + response.credential)
+        let userObj = jwtDecode(response.credential)
+        setUser(userObj)
+        console.log(userObj)
+
+        let data = {
+                name: userObj.given_name,
+                lastName: userObj.family_name,
+                email: userObj.email,
+                password:userObj.sub,
+                country: "undefined",
+                from: 'google',
+                role: 'user',
+                photo: userObj.picture
+        }
+        signup(data).then(res => console.log(res))
+        navigate('/signin')
+    }
+
+    useEffect(()=>{
+        /* global google */
+        google.accounts.id.initialize({
+            client_id: "233988563348-ftfff5fccl2n9035bkl2l7cgsa2cnqme.apps.googleusercontent.com",
+            callback: handleCallbackResponse
+        })
+        google.accounts.id.renderButton(
+            document.getElementById("buttonGoogle"),
+            {size:"medium",logo_alignment: 'center',
+            width: 250, shape: 'circle', type: 'standard'}
+        )
+    },[])
+  return (
+    <div>
+        <div id="buttonGoogle" data-context="use"></div>
+    </div>
+  )
+}
