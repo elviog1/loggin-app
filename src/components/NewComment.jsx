577fe38030f21a21b56506c2c3a8c6ff184ab3d0
Elvio Galeano <elvio.galeano827@gmail.com>
2023-02-22 18:51:56 -0300

    create new comments

---
+++ src/components/NewComment.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { useCreateCommentMutation } from '../features/commentsApi'
+
+export default function NewComment() {
+    const [postComment] = useCreateCommentMutation()
+    const [ message, setMessage] = useState("")
+    const user = JSON.parse(localStorage.getItem('user'))
+    const id = JSON.parse(localStorage.getItem('id'))
+
+    const sendComment = (e)=>{
+        e.preventDefault()
+        let data = {
+            comment: message,
+            user: id
+        }
+        postComment(data).then(res =>{
+            console.log(res)
+        } )
+    }
+  return (
+    <form onSubmit={sendComment}>
+        <span>{user.name}</span>
+        <div style={{display:"flex"}}>
+            <img src={user.picture} />
+            <textarea onChange={(e)=> setMessage(e.target.value)}></textarea>
+        </div>
+        <button >Enviar</button>
+    </form>
+  )
+}
