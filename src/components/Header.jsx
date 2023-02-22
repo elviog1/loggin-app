c1a6729470603b4aa1b4db53d43bc8438d71076b
Elvio Galeano <elvio.galeano827@gmail.com>
2023-02-22 18:52:31 -0300

    header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSignOutMutation } from '../features/usersApi'
 
 export default function Header() {
   const user = JSON.parse(localStorage.getItem("user"))
+  const [email,setEmail] = useState("")
+  const [signout] = useSignOutMutation()
+  const navigate = useNavigate()
 
+  useEffect(()=>{
+    if(user){
+      setEmail(user.email)
+    }
+  },[])
+  const handleSignout = ()=>{
+    signout(email).then(res => {
+      console.log(res)
+      navigate('/')
+      localStorage.removeItem('user')
+      localStorage.removeItem('id')
+    })
+
+  }
+  console.log(user)
   return (
     <header>
-        <span>{user.name}</span>
-        <span>Descontarse</span>
-      </header>
+        <span>Login-APP</span>
+        {user && 
+          <div>
+            <span>{user.name}</span>
+            <span style={{color:'red',cursor:'pointer'}} onClick={handleSignout}>Descontarse</span>
+          </div>
+        }
+    </header>
   )
 }
