11294f1230613fa0b7cb913decab4153a040f5fc
Elvio Galeano <elvio.galeano827@gmail.com>
2023-02-22 18:50:21 -0300

    add header and commentPage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Header from './components/Header'
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
 import Welcome from './components/Welcome'
-import Home from './pages/Home'
+import CommentsPage from './pages/CommentsPage'
+
 
 function App() {
-
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<CommentsPage />} />
       </Routes>
     </BrowserRouter>
 
