461aea638fd5906a48cd24ae9bda3af57e99b95c
Elvio Galeano <elvio.galeano827@gmail.com>
2023-02-24 14:55:07 -0300

    create not found page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export default function NotFound() {
+    const user = JSON.parse(localStorage.getItem("user"))
+    const navigate = useNavigate()
+  return (
+    <main className="min-h-screen items-center bg-blue-300 flex justify-center flex-col gap-5 ">
+        <h1 className='text-xl font-bold'>Oooooooooooooooooops !!!</h1>
+        {user ? <button className='bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded' onClick={()=> navigate('/comments')}>Back</button> : <button className='bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded' onClick={()=> navigate('/signin')}>Sign in</button>}
+    </main>
+  )
+}
