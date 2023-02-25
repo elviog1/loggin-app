4992bbe252b373b6a2725d7427a000d48d0707b9
Elvio Galeano <elvio.galeano827@gmail.com>
2023-02-25 15:01:53 -0300

    fix styles

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,25 +2,31 @@ import React from 'react'
 export default function Comments(props) {
     const commentData = props.comments
     const user = JSON.parse(localStorage.getItem("id"))
-
+    const dateArray = commentData.date.split(",")
+    const time = dateArray[1].slice(1,6)
     const refresh = () =>{
       props.handleDeleteComment()
     }
-
   return (
-    <div className='mb-5 px-4 shadow-lg hover:shadow-2xl'>
-        <h3 className='text-center font-bold text-sm'>{commentData.user.name}</h3>
-        <div className='flex items-center bg-slate-100 rounded'>
-            <img className='w-24 rounded' src={commentData.user.photo} alt='user picture' />
-            <h3 className="px-3 font-serif">{commentData.comment}</h3>
+    <div className='flex gap-2 bg-slate-200 p-3 rounded-xl my-3 max-w-xl shadow'>
+      <div>
+        <img className='w-16 rounded-full' src={commentData.user.photo} alt='user picture' />
+      </div>
+      <div className='flex flex-col w-full'>
+        <div className='flex items-center gap-2'>
+        <span className='text-center font-bold text-sm'>{user === commentData.user._id ? "🙍‍♂️" : "👤"}{commentData.user.name}:</span>
         </div>
+        <h3 className="px-3 font-serif max-w-xl">{commentData.comment}</h3>
         <div className='flex justify-between items-end'>
-          <h4 className="text-end text-gray-500">{commentData.date}</h4>
+          <span className='text-gray-400 text-xs hover:text-gray-500'>{dateArray[0]} - {time}</span>
           {user === commentData.user._id && 
-          <img onClick={refresh} className='w-8 cursor-pointer bg-red-400 hover:bg-red-600 rounded p-0.5' src='https://cdn-icons-png.flaticon.com/512/484/484611.png' alt='delete icon' />
+          <div onClick={refresh} className='p-1.5 font-bold flex cursor-pointer bg-red-400 hover:bg-red-600 rounded transition-colors'>
+            <span>Delete</span>
+            <img className='w-5' src='https://cdn-icons-png.flaticon.com/512/484/484611.png' alt='delete icon' />
+          </div>
           }
-          
         </div>
+      </div>
     </div>
   )
 }
--- src/components/NewComment.jsx
+++ src/components/NewComment.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 export default function NewComment({postComment}) {
     const [ message, setMessage] = useState("")
     const user = JSON.parse(localStorage.getItem('user'))
     const id = JSON.parse(localStorage.getItem('id'))
-
+    const textareaRef = useRef()
     const sendComment = (e)=>{
         e.preventDefault()
         let data = {
@@ -13,17 +13,20 @@ export default function NewComment({postComment}) {
         }
         postComment(data).then(res =>{
             console.log(res)
+            textareaRef.current.value = ""
         } )
     }
-
   return (
-    <form className='flex flex-col px-4' onSubmit={sendComment}>
-        <span className="text-center font-bold">{user.name}</span>
-        <div className='flex'>
-            <img className='rounded w-24' src={user.picture || user.photo} alt='user picture' />
-            <textarea className="resize-none rounded w-full" onChange={(e)=> setMessage(e.target.value)}></textarea>
+    <form className='bg-slate-200 rounded-xl p-3 flex gap-2 items-center shadow' onSubmit={sendComment}>
+        <div>
+            <img className='rounded-full w-16' src={user.picture || user.photo} alt='user picture' />
         </div>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Enviar</button>
+        <div className='w-full'>
+            <textarea ref={textareaRef} className=" resize-none rounded w-full  px-3 pt-1 placeholder-gray-600" onChange={(e)=> setMessage(e.target.value)} placeholder="What's your biggest secret?"></textarea>
+        </div>
+        <div>
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Enviar</button>
+        </div>
     </form>
   )
 }
