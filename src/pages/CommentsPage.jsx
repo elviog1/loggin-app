e4cb26ac290c40fab94bc959cce3b73d98fb560f
Elvio Galeano <elvio.galeano827@gmail.com>
2023-02-24 23:35:51 -0300

    add createComment and deleteComment mutations

--- src/pages/CommentsPage.jsx
+++ src/pages/CommentsPage.jsx
@@ -1,27 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import Comments from '../components/Comments'
 import NewComment from '../components/NewComment'
-import { useAllCommentsQuery } from '../features/commentsApi'
+import { useAllCommentsMutation, useCreateCommentMutation, useDeleteCommentMutation } from '../features/commentsApi'
 
 export default function CommentsPage() {
-    const {data} = useAllCommentsQuery("")
-    const [allComments,setAllComments] = useState([])
+    const [postComment,{data:createData}] = useCreateCommentMutation()
+    const [comments] = useAllCommentsMutation("")
+    const [deleteComment,{data}] = useDeleteCommentMutation()
+    const [allComments,setAllComments] = useState()
+    const [refresh,setRefresh] = useState(false)
 
     useEffect(()=>{
-        if(data){
-            setAllComments(data)
-        }
-    },[data])
-    console.log(data)   
+      comments().then(res => {
+            setAllComments(res.data)
+         })
+         .catch(error => console.log(error))
+    },[data,createData])
+
+    const handleDeleteComment = async(idComment)=>{
+      await deleteComment(idComment)
+      setRefresh(()=> {return !refresh})
+    }
+
   return (
     <main className='flex items-center justify-center bg-blue-300 min-h-screen'>
       <div className=''>
         <h1 className='text-center font-bold text-2xl'>Comments</h1> 
         {allComments?.map((comment,index) =>(
-          <Comments comments={comment} key={index}  />
+          <Comments comments={comment} key={index} handleDeleteComment={()=>handleDeleteComment(comment._id)}  />
           ))}
-        <NewComment />
-          </div>
+        <NewComment postComment={postComment} />
+      </div>
     </main>
   )
 }
